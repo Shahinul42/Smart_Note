db44a697e3717df7e7b4e6a93162eff8946e00a3
siam <siampatoary985@gmail.com>
2022-04-20 00:07:17 +0600

    final cromic  nong

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,17 +1,17 @@
 console.log("Welcome to smart note")
 
 let body = document.querySelector("body");
-let Cro = document.querySelector(".Cro");
+let Cro = document.querySelector("section.Cro");
+let a = 1;
 
 
-body.addEventListener("dblclick", ()=>{
+body.addEventListener("dblclick", () => {
     console.log("dbl click success")
-    // CREATE DYNAMIC_ROW CLASS
-    let Dynamic_row = document.createElement("div");
-    Cro.appendChild(Dynamic_row);
-    Dynamic_row = Dynamic_row.classList.add("Dynamic_row")
-    ///////////////////
-    let Strong = document.createElement("strong")
-    Dynamic_row.appendChild(Strong);
+    // CREATE DYNAMIC_strong element
+    let Dynamic_row_strong = document.createElement("strong");
+    Cro.appendChild(Dynamic_row_strong);
+    Dynamic_row_strong.classList.add("Dynamic_row");
+    a = a+1;
+    Dynamic_row_strong.innerText = a;
 
 })
